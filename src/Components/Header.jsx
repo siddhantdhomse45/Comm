cf811283e270cec31c65b496300d3a5423a6ae47
siddhantdhomse45 <siddh654@gmail.com>
2024-12-07 12:34:41 -0500

    six commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,113 +1,126 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { FaSearch, FaSignInAlt, FaShoppingCart, FaHeart } from 'react-icons/fa';
 
-
 const Header = () => {
-   
+    const [navActive, setNavActive] = useState(false);
+
+    const toggleNavbar = () => {
+        setNavActive(!navActive);
+    };
+
     return (
         <div>
             <header className="header" data-header>
                 <div className="container">
-
-                    <div className="overlay" data-overlay></div>
-
+                    {/* Header Search */}
                     <div className="header-search">
-                        <input type="search" name="search" placeholder="Search Product..." className="input-field"/>
-
-                            <button className="search-btn" aria-label="Search">
-                                <FaSearch name="search-outline"></FaSearch>
-                            </button>
+                        <input
+                            type="search"
+                            name="search"
+                            placeholder="Search Product..."
+                            className="input-field"
+                        />
+                        <button className="search-btn" aria-label="Search">
+                            <FaSearch />
+                        </button>
                     </div>
 
+                    {/* Logo */}
                     <a href="#" className="logo">
-                        <img src="https://casmart.vercel.app/assets/images/logo.svg" alt="Casmart logo" width="130" height="31"/>
+                        <img
+                            src="https://casmart.vercel.app/assets/images/logo.svg"
+                            alt="Casmart logo"
+                            width="130"
+                            height="31"
+                        />
                     </a>
 
+                    {/* Header Actions */}
                     <div className="header-actions">
-
                         <button className="header-action-btn">
-                            {/* <ion-icon name="person-outline" aria-hidden="true"></ion-icon> */}
-                            <FaSignInAlt></FaSignInAlt>
+                            <FaSignInAlt />
                             <p className="header-action-label">Sign in</p>
                         </button>
 
                         <button className="header-action-btn">
-                            <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
-                          
-                            <p className="header-action-label">Search</p>
-                        </button>
-
-                        <button className="header-action-btn">
-                            {/* <ion-icon name="cart-outline" aria-hidden="true"></ion-icon> */}
-                              <FaShoppingCart></FaShoppingCart>
-
+                            <FaShoppingCart />
                             <p className="header-action-label">Cart</p>
-
-                            <div className="btn-badge green" aria-hidden="true">3</div>
+                            <div className="btn-badge green" aria-hidden="true">
+                                3
+                            </div>
                         </button>
 
                         <button className="header-action-btn">
-                            {/* <ion-icon name="heart-outline" aria-hidden="true"></ion-icon> */}
-                            <FaHeart></FaHeart>
-
-                            <p className="header-action-label">Wishlisht</p>
-
-                            <div className="btn-badge" aria-hidden="true">2</div>
+                            <FaHeart />
+                            <p className="header-action-label">Wishlist</p>
+                            <div className="btn-badge" aria-hidden="true">
+                                2
+                            </div>
                         </button>
-
                     </div>
 
-                    <button className="nav-open-btn" data-nav-open-btn aria-label="Open Menu">
+                    {/* Mobile Menu Button */}
+                    <button
+                        className="nav-open-btn"
+                        aria-label="Open Menu"
+                        onClick={toggleNavbar} 
+                    >
+                        {/* <span></span>
                         <span></span>
-                        <span></span>
-                        <span></span>
+                        <span></span> */}
+                        <ion-icon name="menu-outline"></ion-icon>
                     </button>
 
-                    <nav className="navbar" data-navbar>
-
+                    {/* Navbar */}
+                    <nav className={`navbar ${navActive ? 'active' : ''}`} data-navbar>
                         <div className="navbar-top">
-
                             <a href="#" className="logo">
-                                <img src="./assets/images/logo.svg" alt="Casmart logo" width="130" height="31"/>
+                                <img
+                                    src="https://casmart.vercel.app/assets/images/logo.svg"
+                                    alt="Casmart logo"
+                                    width="130"
+                                    height="31"
+                                />
                             </a>
-
-                            <button className="nav-close-btn" data-nav-close-btn aria-label="Close Menu">
+                           <button
+                                className="nav-close-btn"
+                                aria-label="Close Menu"
+                                onClick={toggleNavbar}
+                            >
                                 <ion-icon name="close-outline"></ion-icon>
                             </button>
-
-                        </div>
+                        </div> 
 
                         <ul className="navbar-list">
-
                             <li>
-                                <a href="#home" className="navbar-link">Home</a>
+                                <a href="#home" className="navbar-link">
+                                    Home
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#" className="navbar-link">Shop</a>
+                                <a href="#" className="navbar-link">
+                                    Shop
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#" className="navbar-link">About</a>
+                                <a href="#" className="navbar-link">
+                                    About
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#blog" className="navbar-link">Blog</a>
+                                <a href="#blog" className="navbar-link">
+                                    Blog
+                                </a>
                             </li>
-
                             <li>
-                                <a href="#" className="navbar-link">Contact</a>
+                                <a href="#" className="navbar-link">
+                                    Contact
+                                </a>
                             </li>
-
                         </ul>
-
                     </nav>
-
                 </div>
             </header>
-
-
         </div>
     );
 };
